53f5e6f03a3fde14d509b94eed222a3d15d4d83f
IrzaB <irzabari098@gmail.com>
2024-07-16 06:45:08 -0700

    Add files via upload

---
+++ A-19/index.js
@@ -0,0 +1,5 @@
+"use strict";
+// Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating 
+// the number of people you are inviting to dinner.
+let names = ["Sidra", "Mariam", "Laiba"];
+console.log(`Total number of invited guests: ${names.length}`);
---
+++ A-19/index.ts
@@ -0,0 +1,7 @@
+// Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating 
+// the number of people you are inviting to dinner.
+
+
+let names = ["Sidra", "Mariam", "Laiba"];
+
+console.log(`Total number of invited guests: ${names.length}`);
