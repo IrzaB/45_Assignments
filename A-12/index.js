f4b739dee6e2238b8e1be0de9ddde55a35a20f8c
IrzaB <irzabari098@gmail.com>
2024-07-16 06:39:53 -0700

    Add files via upload

---
+++ A-12/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name,
+//print a message to them. The text of each message should be the same, but each message should be personalized
+//with the person’s name.
+let names = ["Irza", "Faiza", "Sumaiya", "Laiba"];
+console.log(`${names[0]} welcome to my home.`);
+console.log(`${names[1]} welcome to my home.`);
+console.log(`${names[2]} welcome to my home.`);
+console.log(`${names[3]} welcome to my home.`);
---
+++ A-12/index.ts
@@ -0,0 +1,8 @@
+//Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name,
+//print a message to them. The text of each message should be the same, but each message should be personalized
+//with the person’s name.
+let names: string[] = ["Irza","Faiza", "Sumaiya", "Laiba"];
+console.log(`${names[0]} welcome to my home.`);
+console.log(`${names[1]} welcome to my home.`);
+console.log(`${names[2]} welcome to my home.`);
+console.log(`${names[3]} welcome to my home.`);
