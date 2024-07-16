340bc688920f3e4c2b81ae9c6ef73c1d22445adc
IrzaB <irzabari098@gmail.com>
2024-07-16 06:22:16 -0700

    Add files via upload

---
+++ A-05/index.js
@@ -0,0 +1,7 @@
+"use strict";
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called
+//famous_person. Then compose your message and store it in a new variable called message. Print your message.
+Object.defineProperty(exports, "__esModule", { value: true });
+let Famous_person = "Hazrat Umar Bin Khattab (R.A)";
+let message = "“Patience is a pillar of faith”.";
+console.log(`${Famous_person} said, ${message}`);
---
+++ A-05/index.ts
@@ -0,0 +1,6 @@
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called
+//famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+let Famous_person: string = "Hazrat Umar Bin Khattab (R.A)";
+let message: string = "“Patience is a pillar of faith”.";
+console.log(`${Famous_person} said, ${message}`);
