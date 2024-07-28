8b8e1623dfc25f992b47ec5b2b536ca1efa4aaa2
IrzaB <irzabari098@gmail.com>
2024-07-28 04:30:01 -0700

    Add files via upload

---
+++ A-41/index.js
@@ -0,0 +1,11 @@
+"use strict";
+// Magicians: Make a array of magician’s names. Pass the array to a function called show_magicians(), which prints the name of each
+// magician in the array.
+Object.defineProperty(exports, "__esModule", { value: true });
+function show_magicians(magicians) {
+    for (const magician of magicians) {
+        console.log(magician);
+    }
+}
+const magicianName = ["Doug Henning", "Max Maven", "Penn"];
+show_magicians(magicianName);
